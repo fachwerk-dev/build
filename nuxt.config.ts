252cd4c634d95d0e29c74d1e4db7acefc2eb9b89
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-09-26 07:58:35 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/google-fonts",
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
-    "@nuxtjs/strapi"
-  ]
-})
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/strapi"],
+});
